5c1bae5ef01082c74ec8fcf13e94625abb9d2bfd
Bikku B S <108191121+bikkubs97@users.noreply.github.com>
2022-08-14 14:22:38 +0530

    update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://f76f24a8.us-south.apigw.appdomain.cloud/guestbook';
+const apiUrl = 'https://55c7acdf.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
